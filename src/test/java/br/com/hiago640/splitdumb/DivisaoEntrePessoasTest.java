fc4b101fb4697c60c7186ac99b0c4b47c3283631
hiago640 <hiago_pereiraborges@hotmail.com>
2023-12-20 10:51:09 -0300

    refactor: utilizar oo para realizar os testes

--- src/test/java/br/com/hiago640/splitdumb/DivisaoEntrePessoasTest.java
+++ src/test/java/br/com/hiago640/splitdumb/DivisaoEntrePessoasTest.java
@@ -1,114 +1,109 @@
 package br.com.hiago640.splitdumb;
 
+import java.util.Vector;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 class Person {
-    String name;
-    int balance;
+	String name;
+	double balance;
 
-    public Person(String name) {
-        this.name = name;
-        this.balance = 0;
-    }
+	public Person(String name) {
+		this.name = name;
+		this.balance = 0;
+	}
 }
 
 @TestInstance(Lifecycle.PER_CLASS)
 class DivisaoEntrePessoasTest {
 
 	static final int N = 4;
-	private Person[] people = new Person[N];
-	
+	private Vector<Person> people = new Vector<>();
+
 	@BeforeAll
 	void populaPessoas() {
-        people[0] = new Person("Hiago");
-        people[1] = new Person("Leandro");
-        people[2] = new Person("Antônio");
-        people[3] = new Person("Tayna");
+		people.add(new Person("Hiago"));
+		people.add(new Person("Leandro"));
+		people.add(new Person("Antônio"));
+		people.add(new Person("Tayna"));
 	}
-	
+
 	@BeforeAll
 	void populaSaldo() {
-        // Definir os débitos e créditos
-        people[0].balance -= 10;
-        people[0].balance -= 30;
-        people[0].balance -= 20;
-        people[0].balance += 30;
-        people[0].balance += 30;
-        people[0].balance += 30;
+		// Definir os débitos e créditos
+		people.get(0).balance -= 10;
+		people.get(0).balance -= 30;
+		people.get(0).balance -= 20;
+		people.get(0).balance += 30;
+		people.get(0).balance += 30;
+		people.get(0).balance += 30;
 
-        people[1].balance -= 30;
-        people[1].balance -= 30;
-        people[1].balance += 10;
-        people[1].balance += 10;
-        people[1].balance += 10;
+		people.get(1).balance -= 30;
+		people.get(1).balance -= 30;
+		people.get(1).balance += 10;
+		people.get(1).balance += 10;
+		people.get(1).balance += 10;
 
-        people[2].balance -= 30;
-        people[2].balance -= 10;
-        people[2].balance += 30;
-        people[2].balance += 30;
+		people.get(2).balance -= 30;
+		people.get(2).balance -= 10;
+		people.get(2).balance += 30;
+		people.get(2).balance += 30;
 
-        people[3].balance -= 30;
-        people[3].balance -= 10;
-        people[3].balance += 20;
+		people.get(3).balance -= 30;
+		people.get(3).balance -= 10;
+		people.get(3).balance += 20;
 	}
-	
-    // Função utilitária para encontrar o índice do valor mínimo em um array
-    static int getMin(Person[] people) {
-        int minInd = 0;
-        for (int i = 1; i < N; i++)
-            if (people[i].balance < people[minInd].balance)
-                minInd = i;
-        return minInd;
-    }
 
-    // Função utilitária para encontrar o índice do valor máximo em um array
-    static int getMax(Person[] people) {
-        int maxInd = 0;
-        for (int i = 1; i < N; i++)
-            if (people[i].balance > people[maxInd].balance)
-                maxInd = i;
-        return maxInd;
-    }
+	// Função utilitária para encontrar o índice do valor mínimo em um array
+	static Person getMin(Vector<Person> people) {
+		return people.stream().min((p1, p2) -> Double.compare(p1.balance, p2.balance)).orElse(null);
+	}
 
-    // Função utilitária para retornar o mínimo entre dois valores
-    static int minOf2(int x, int y) {
-        return (x < y) ? x : y;
-    }
+	// Função utilitária para encontrar o índice do valor máximo em um array
+	static Person getMax(Vector<Person> people) {
+		return people.stream().max((p1, p2) -> Double.compare(p1.balance, p2.balance)).orElse(null);
+	}
 
-    // Função recursiva para calcular o fluxo de caixa mínimo
-    static void minCashFlowRec(Person[] people) {
-        // Encontrar os índices da pessoa que deve pagar o valor máximo (mxCredit)
-        // e da pessoa que deve receber o valor máximo (mxDebit)
-        int mxCredit = getMax(people), mxDebit = getMin(people);
+	// Função utilitária para retornar o mínimo entre dois valores
+	static double minOf2(double x, double y) {
+		return (x < y) ? x : y;
+	}
 
-        // Se ambos os valores são zero, todas as dívidas foram liquidadas
-        if (people[mxCredit].balance == 0 && people[mxDebit].balance == 0)
-            return;
+	// Função recursiva para calcular o fluxo de caixa mínimo
+	static void minCashFlowRec(Vector<Person> people) {
+		// Encontrar os índices da pessoa que deve pagar o valor máximo (mxCredit)
+		// e da pessoa que deve receber o valor máximo (mxDebit)
+		Person personWithMaxCredit = getMax(people);
+		Person personWithMaxDebit = getMin(people);
 
-        // Calcular o mínimo entre os dois valores e atualizar os saldos das pessoas
-        int min = minOf2(-people[mxDebit].balance, people[mxCredit].balance);
-        people[mxCredit].balance -= min;
-        people[mxDebit].balance += min;
+		// Se ambos os valores são zero, todas as dívidas foram liquidadas
+		if (personWithMaxCredit.balance == 0 && personWithMaxDebit.balance == 0)
+			return;
 
-        // Imprimir os detalhes da transação
-        System.out.println(people[mxDebit].name + " paga " + min + " para " + people[mxCredit].name);
+		// Calcular o mínimo entre os dois valores e atualizar os saldos das pessoas
+		double min = minOf2(-personWithMaxDebit.balance, personWithMaxCredit.balance);
+		personWithMaxCredit.balance -= min;
+		personWithMaxDebit.balance += min;
 
-        // Recursão para o array de pessoas
-        minCashFlowRec(people);
-    }
+		// Imprimir os detalhes da transação
+		System.out.println(personWithMaxDebit.name + " paga " + min + " para " + personWithMaxCredit.name);
 
-    // Função principal para encontrar o fluxo de caixa mínimo
-    static void minCashFlow(Person[] people) {
-        // Chamar a função recursiva para encontrar o fluxo de caixa mínimo
-        minCashFlowRec(people);
-    }
-	
+		// Recursão para o array de pessoas
+		minCashFlowRec(people);
+	}
+
+	// Função principal para encontrar o fluxo de caixa mínimo
+	static void minCashFlow(Vector<Person> people) {
+		// Chamar a função recursiva para encontrar o fluxo de caixa mínimo
+		minCashFlowRec(people);
+	}
+
 	@Test
 	void test() {
-        minCashFlow(people);
+		minCashFlow(people);
 	}
 
 }
